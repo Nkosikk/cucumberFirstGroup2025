64ee93f5381ce82631f482d8d61bfe1e12eb986d
sibonisot <sibonisot@yahoo.com>
2025-03-25 19:28:07 +0200

    created an extent report

--- src/test/java/Runner/runner.java
+++ src/test/java/Runner/runner.java
@@ -7,8 +7,8 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "src/test/resources/Features",
         glue = {"Steps"},
-        tags = "@PurchaseItem",
-        plugin = {"html:target/cucumber-report.html"},
+        tags = "@Regression",
+        plugin = {"html:target/cucumber-report.html","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
         monochrome = true,
         publish = true)
 public class runner extends AbstractTestNGCucumberTests {
