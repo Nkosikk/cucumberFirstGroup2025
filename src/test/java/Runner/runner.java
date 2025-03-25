b6873fa55b6cd3734f9c754d341dc51c85687eec
sibonisot <sibonisot@yahoo.com>
2025-03-25 18:49:27 +0200

    configured the report

--- src/test/java/Runner/runner.java
+++ src/test/java/Runner/runner.java
@@ -7,8 +7,8 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "src/test/resources/Features",
         glue = {"Steps"},
-        tags = "",
-        plugin = {"pretty"},
+        tags = "@PurchaseItem",
+        plugin = {"html:target/cucumber-report.html"},
         monochrome = true,
         publish = true)
 public class runner extends AbstractTestNGCucumberTests {
