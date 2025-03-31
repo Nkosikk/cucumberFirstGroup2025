8d6eaca43bd4ef966249214a33d641377cb94043
LNtanjana <lindantanjana@gmail.com>
2025-03-31 15:46:31 +0200

    updated the runner

--- src/test/java/Runner/runner.java
+++ src/test/java/Runner/runner.java
@@ -1,4 +1,13 @@
 package Runner;
 
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = {"StepDefinitions"},
+        plugin = {"pretty", "html:target/cucumber-reports"},
+        monochrome = true
+)
+
 public class runner {
 }
